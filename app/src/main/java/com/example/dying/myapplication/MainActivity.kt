0bf7ad9765ee495f5fc295ba9893137a1731588e
itsumoonazicode <dyingkouki@gmail.com>
2018-10-14 16:34:16 +0900

    ログを表示させるコード追加もエラー発生状態

--- app/src/main/java/com/example/dying/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/dying/myapplication/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.dying.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val value1: int = 1
+        val value2: int = 2
+
+        val value: double = value1 / value2
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setText(String.valueOf(value))
+
+        val editText = findViewById<EditText>(R.id.editText)
+        editText.setText("EditTextに文字列を表示！")
+
+        val string: String = editText.getText().toString()
+        Log.d("EditTextTest", string)
     }
 }
